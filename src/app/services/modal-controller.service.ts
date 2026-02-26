3ec0624ed22d2eeea8fba799c4402d4e9e795377
Gleydsong <ggleydson@icloud.com>
2026-02-26 22:16:37 +0000

    refactor: remove unused modal components and implement modal controller service

---
+++ src/app/services/modal-controller.service.ts
@@ -0,0 +1,33 @@
+import { inject, Injectable } from '@angular/core';
+import { Dialog } from '@angular/cdk/dialog';
+import { TaskFormModalComponent } from '../components/task-form-modal/task-form-modal.component';
+import { TaskCommentsModalComponent } from '../components/task-comments-modal/task-comments-modal.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalControllerService {
+  private readonly modalSizeOpction = {
+    maxWidth: '620px',
+    width: '100%',
+  };
+  private readonly _dialog = inject(Dialog);
+
+  openNewTaskModal() {
+    return this._dialog.open(TaskFormModalComponent, {
+      ...this.modalSizeOpction,
+    });
+  }
+
+  openEditTaskModal() {
+    return this._dialog.open(TaskFormModalComponent, {
+      ...this.modalSizeOpction,
+    });
+  }
+
+  openTaskCommentsModal() {
+    return this._dialog.open(TaskCommentsModalComponent, {
+      ...this.modalSizeOpction,
+    });
+  }
+}
